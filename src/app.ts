0068594c0a9f387b0d2ef9815ddf77fe7a030064
Kishan Sharma <kishan.sharma@stitchmes.com>
2025-11-20 01:30:05 +0530

    setup express server

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+   res.json('Hello world');
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
+import app from './app.js';
 import { Config } from './config/index.js';
 
-const testFunction = (name: string) => {
-   console.log(`abc ${name}, ${Config.PORT}!`);
+const startServer = () => {
+   try {
+      app.listen(Config.PORT, () => {
+         console.log(`Server listening at port ${Config.PORT}`);
+      });
+   } catch (e) {
+      console.log('server error: ', e);
+   }
 };
 
-testFunction('world');
+startServer();
