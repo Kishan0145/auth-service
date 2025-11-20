69270e864df7229f1a1945aa8b00118d7e7f98b6
Kishan Sharma <kishan.sharma@stitchmes.com>
2025-11-20 23:24:07 +0530

    add logger

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
 import { config } from 'dotenv';
 config();
 
-const { PORT,LOG_LEVEL,NODE_ENV } = process.env;
+const { PORT, LOG_LEVEL, NODE_ENV } = process.env;
 export const Config = {
    PORT,
    LOG_LEVEL,
-   NODE_ENV
+   NODE_ENV,
 };
--- src/config/logger.ts
+++ src/config/logger.ts
@@ -3,28 +3,25 @@ import { Config } from './index.js';
 import 'winston-daily-rotate-file';
 
 const fileRotateTransportForErrors = new winston.transports.DailyRotateFile({
-   filename:'logs/app-error-%DATE%.log',
-   datePattern:'YYYY-MM-DD',
-   level:'error',
-   maxSize:'20m',
-   maxFiles:'14d'
-})
+   filename: 'logs/app-error-%DATE%.log',
+   datePattern: 'YYYY-MM-DD',
+   level: 'error',
+   maxSize: '20m',
+   maxFiles: '14d',
+});
 
 const fileRotateTransportForCombine = new winston.transports.DailyRotateFile({
-   filename:'logs/app-combine-%DATE%.log',
-   datePattern:'YYYY-MM-DD',
-   maxSize:'20m',
-   maxFiles:'14d'
-})
+   filename: 'logs/app-combine-%DATE%.log',
+   datePattern: 'YYYY-MM-DD',
+   maxSize: '20m',
+   maxFiles: '14d',
+});
 
-const {combine,timestamp,json} = winston.format
+const { combine, timestamp, json } = winston.format;
 const logger = winston.createLogger({
-   level:Config.LOG_LEVEL || 'info',
-   format:combine(timestamp(),json()),
-   transports:[
-      fileRotateTransportForErrors,
-      fileRotateTransportForCombine
-   ]
-})
+   level: Config.LOG_LEVEL || 'info',
+   format: combine(timestamp(), json()),
+   transports: [fileRotateTransportForErrors, fileRotateTransportForCombine],
+});
 
 export default logger;
