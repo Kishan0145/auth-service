c805da48620d703154474e91448a95fff175fe40
Kishan Sharma <kishan.sharma@stitchmes.com>
2025-11-24 22:30:45 +0530

    setup postgres

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,11 @@
 import { config } from 'dotenv';
-config();
+import { join } from 'path';
+
+const path = join(
+   import.meta.dirname,
+   `../../.env.${process.env.NODE_ENV || 'dev'}`
+);
+config({ path: path });
 
 const { PORT, LOG_LEVEL, NODE_ENV } = process.env;
 export const Config = {
--- src/controllers/users/user.controler.ts
+++ src/controllers/users/user.controler.ts
@@ -1,7 +1,15 @@
-import type { Request, Response } from 'express';
+import type { NextFunction, Request, Response } from 'express';
 
-const registerController = (req: Request, res: Response) => {
-   return res.status(201).json({});
+const registerController = (
+   req: Request,
+   res: Response,
+   next: NextFunction
+) => {
+   try {
+      return res.status(201).json({});
+   } catch (e) {
+      next(e);
+   }
 };
 
 const usersControllers = {
